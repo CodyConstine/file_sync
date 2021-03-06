b246bd41775ed15e8ea0cce2492ae5039bb450b3
Cody Constine <cody.constine@colorado.edu>
2021-03-05 21:15:29 -0700

    Moving to async for everything

--- src/gist_io.rs
+++ src/gist_io.rs
@@ -54,34 +54,33 @@ impl GistIo {
         }
     }
 
-    pub fn find_gist(&self, gist_name: &str) -> Option<Gist> {
+    pub async fn find_gist(&self, gist_name: &str) -> Result<Gist, Error> {
         let rt = Runtime::new().unwrap();
-        let resp_built = self.client.get(&format!("{}/gists", &self.url))
+        let response = self.client.get(&format!("{}/gists", &self.url))
             .header("Authorization", format!("token {}", &self.git_token))
-            .header("user-agent", "reqwest/0.11.1");
-        let response = rt.block_on(resp_built.send());
-        let response_contents = match response {
+            .header("user-agent", "reqwest/0.11.1").send();
+        let response_contents = match response.await {
             Ok(res) => {
                 match rt.block_on(res.json::<Vec<Gist>>()) {
                     Ok(str) => str,
                     Err(e) => {
                         println!("{}", e.to_string());
-                        return None;
+                        return Err(Error);
                     }
                 }
             }
-            Err(e) => return None,
+            Err(e) => return Err(Error),
         };
 
         for gist in response_contents {
             if gist.files.contains_key(gist_name) {
-                return Some(gist);
+                return Ok(gist);
             }
         }
-        return None;
+        return Err(Error)
     }
 
-    pub fn write_gist(&self, contents: &str, file_name: &str, gist: &Gist) -> Result<(), Error> {
+    pub async fn write_gist(&self, contents: &str, file_name: &str, gist: &Gist) -> Result<(), Error> {
         let gist_body = json!({
         "files": {
              file_name: {
@@ -93,14 +92,14 @@ impl GistIo {
             .header("user-agent", "reqwest/0.11.1")
             .json(&gist_body).send();
 
-        let rt = Runtime::new().unwrap();
-        match rt.block_on(response) {
+        match response.await {
             Ok(_) => Ok(()),
+            // TODO figure out how to make an error
             Err(_) => Err(Error),
         }
     }
 
-    pub fn create_gist(&self, contents: &str, file_name: &str) -> Result<(), Error> {
+    pub async fn create_gist(&self, contents: &str, file_name: &str) -> Result<(), Error> {
         let gist_body = json!({
         "files": {
              file_name: {
@@ -112,8 +111,7 @@ impl GistIo {
             .header("user-agent", "reqwest/0.11.1")
             .json(&gist_body).send();
 
-        let rt = Runtime::new().unwrap();
-        match rt.block_on(response) {
+        match response.await {
             Ok(_) => Ok(()),
             Err(_) => Err(Error),
         }
@@ -123,156 +121,10 @@ impl GistIo {
         format!("file_sync_{}", name)
     }
 
-    pub fn check_if_exists(&self, gist_name: &str) -> bool {
-        self.find_gist(gist_name).is_some()
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::gist_io::GistIo;
-    use reqwest::Client;
-
-    #[test]
-    fn test_make_gist_name() {
-        let results = GistIo::make_gist_name("test_file");
-        let expected = String::from("file_sync_test_file");
-
-        assert_eq!(results, expected);
-    }
-
-    #[test]
-    fn test_check_if_exists() {
-        let url = mockito::server_url();
-        let gist_name = String::from("test_gist");
-        let client = Client::new();
-        let token = String::from("token");
-        let test = GistIo {
-            client,
-            url,
-            git_token: token,
-        };
-
-        let _m = mockito::mock("GET", "/gists")
-            .with_status(201)
-            .with_header("content-type", "text/plain")
-            .with_header("x-api-key", "1234")
-            .with_body(r#"
-                [
-                  {
-                    "id": "aa5a315d61ae9438b18d",
-                    "files": {
-                      "hello_world.rb": {
-                        "filename": "hello_world.rb"
-                      }
-                    },
-                    "truncated": false
-                  },
-                  {
-                    "id": "aa5a315d61ae9438b18d",
-                    "files": {
-                      "test_gist": {
-                        "filename": "test_gist"
-                      }
-                    },
-                    "truncated": false
-                  }
-                ]
-            "#)
-            .create();
-
-        let results = test.check_if_exists("test_gist");
-
-        assert!(results);
-    }
-
-    #[test]
-    fn test_check_if_does_not_exists() {
-        let url = mockito::server_url();
-        let client = Client::new();
-        let token = String::from("token");
-        let test = GistIo {
-            client,
-            url,
-            git_token: token,
-        };
-
-        let _m = mockito::mock("GET", "/gists")
-            .with_status(201)
-            .with_header("content-type", "text/plain")
-            .with_header("x-api-key", "1234")
-            .with_body(r#"
-                [
-                  {
-                    "id": "aa5a315d61ae9438b18d",
-                    "files": {
-                      "hello_world.rb": {
-                        "filename": "hello_world.rb"
-                      }
-                    },
-                    "truncated": false
-                  },
-                  {
-                    "id": "aa5a315d61ae9438b18d",
-                    "files": {
-                      "test_gist": {
-                        "filename": "test_gist"
-                      }
-                    },
-                    "truncated": false
-                  }
-                ]
-            "#)
-            .create();
-
-        let results = test.check_if_exists("test_gist_not_real");
-
-        assert!(!results);
-    }
-
-    #[test]
-    fn test_find_gist() {
-        let url = mockito::server_url();
-        let gist_name = String::from("test_gist");
-        let client = Client::new();
-        let token = String::from("token");
-        let test = GistIo {
-            client,
-            url,
-            git_token: token,
-        };
-        let _m = mockito::mock("GET", "/gists")
-            .with_status(201)
-            .with_header("content-type", "text/plain")
-            .with_header("x-api-key", "1234")
-            .with_body(r#"
-                [
-                  {
-                    "id": "aa5a315d61ae9438b18d",
-                    "files": {
-                      "hello_world.rb": {
-                        "filename": "hello_world.rb"
-                      }
-                    },
-                    "truncated": false
-                  },
-                  {
-                    "id": "aa5a315d61ae9438b18d",
-                    "files": {
-                      "test_gist": {
-                        "filename": "test_gist"
-                      }
-                    },
-                    "truncated": false
-                  }
-                ]
-            "#)
-            .create();
-        let results = match test.find_gist(&gist_name) {
-            Some(str) => str.id,
-            None => String::from("NONE"),
-        };
-        let expected = String::from("aa5a315d61ae9438b18d");
-        assert_eq!(results, expected)
+    pub async fn check_if_exists(&self, gist_name: &str) -> bool {
+        match self.find_gist(gist_name).await {
+            Ok(_) => return true,
+            Err(_) => return false,
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use crate::file_io::read_file;
 use std::env;
-use tokio::runtime::Runtime;
-
+use futures::executor::block_on;
 mod file_io;
 mod gist_io;
 mod file_syncer;
@@ -9,13 +8,9 @@ mod file_syncer;
 fn main() {
     let gist_io = gist_io::GistIo::new(&env::var("GIT_TOKEN").unwrap());
 
-    println!("{:?}", &gist_io);
-    println!("{}", &gist_io.check_if_exists("file_sync_test"));
-
-    let rt = Runtime::new().unwrap();
-    let gist = match gist_io.find_gist("file_sync_test") {
-        Some(gist) => gist,
-        None => return,
+    let gist = match block_on(gist_io.find_gist("file_sync_test")) {
+        Ok(gist) => gist,
+        Err(_) => return,
     };
     gist_io.write_gist("Writing Test Data", "file_sync_test", &gist);
 }
